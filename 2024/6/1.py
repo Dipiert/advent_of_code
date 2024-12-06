5d6150dbbf3db4d604b2d741ba524c78d884b305
Damian Rotta <dipiert@gmail.com>
2024-12-06 07:53:23 -0300

    Pushing WIP

--- 2024/6/1.py
+++ 2024/6/1.py
@@ -18,15 +18,16 @@ def guard_pos(input):
             return i, l.index(GUARD)
         
 def trace(starting_point, obstacle, input, direction):
-    visited = input[:]
+    #visited = input[:]
     if direction == 'up':
         for i in range(1, starting_point[0]): # skip the obstacle itself
-            visited[i] = "".join([visited[i][:starting_point[1]], "X", visited[i][starting_point[1]+1:]])
+            input[i] = "".join([input[i][:starting_point[1]], "X", input[i][starting_point[1]+1:]])
             
     if direction == "right":
-                row = "".join([row[:starting_point[1]], "X" * (end - starting_point[1]), row[end:]])
+        row = input[starting_point[0]+1] 
+        input[starting_point[0]+1]  = "".join([row[:starting_point[1]], "X" * (obstacle - starting_point[1]), row[obstacle:]])
 
-    #print("\n".join(visited))
+        print("\n".join(input))
         
 def search_obstacle(starting_point, input, direction):
     if direction == "up":
@@ -34,15 +35,16 @@ def search_obstacle(starting_point, input, direction):
             if input[i][starting_point[1]] == '#' or i == 0:
                 return (i, starting_point[1])
     
-    if direction == 'right':
-        row = input[starting_point[0]+1] # skip the obstacle
-        if "#" in row:
-            end = row.index("#")
-        else:
-            end = len(row)
+    if direction == 'right': # THIS IS WRONG
+        for j in range(starting_point[1], len(input[0])):
+            row = input[j] # skip the obstacle
+            return (j, row.index("#") if "#" in row else len(row))
+        #if "#" in row:
+        ##    end = row.index("#")
+        #else:
+        #    end = len(row)
         #print(end)
         #print(row)
-        #for j in range(starting_point[1], len(input[0])):
         #    print(row[j])
             #print(f"Found Obstacle at {obstacle}")
             #trace(starting_point, obstacle, input, direction)
@@ -53,7 +55,8 @@ def walk(starting_point, input):
     print(f"Found Obstacle at {obstacle}")
     trace(starting_point, obstacle, input, direction)
     direction = "right"
-    obstacle = search_obstacle(obstacle, input, direction)
+    obstacle = search_obstacle(obstacle, input, direction) 
+    trace(starting_point, obstacle, input, direction)
     
     
         #if visited[i][starting_point[1]] == '#':
