831635aadbd9da623312f1f9ef8719b2af0e25cb
Rotta, Damian P <damian.p.rotta@jpmchase.com>
2024-12-13 09:03:41 -0300

    wip

--- 2024/12/1.py
+++ 2024/12/1.py
@@ -1,11 +1,19 @@
 from collections import defaultdict
 
-area_perimeter_by_plant = defaultdict(int)
+area_by_plant = defaultdict(int)
+perimeter_by_plant = defaultdict(int)
 
-input = """AAAA
-BBCD
-BBCC
-EEEC
+# input = """AAAA
+# BBCD
+# BBCC
+# EEEC
+# """
+
+input = """OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
 """
 
 def transpose(input):
@@ -20,8 +28,6 @@ def transpose(input):
     return t
 
 def calc_area(i, j, input, t):
-    if input[i][j] == 'C':
-        print()
     print(f"Horizontal: {input[i][j]}. Area: {input[i].count(input[i][j])}")
     print(f"Vertical: {input[i][j]}. Area: {t[j].count(input[i][j])}")
     print()
@@ -29,27 +35,17 @@ def calc_area(i, j, input, t):
 
 def how_many_around(plant, input, i, j):
     qty = 0
-    if plant != input[i][j-1] or j == 0:
+    if j == 0 or plant != input[i][j-1]:
         qty += 1
-    if plant != input[i][j+1] or j == len(input):
+    if j == len(input)-1 or plant != input[i][j+1]:
         qty += 1
-    if plant != input[i-1][j] or i == 0:
+    if i == 0 or plant != input[i-1][j]:
         qty +=1
-    if plant != input[i+1][j] or i < len(input[0]):
+    if i == len(input[0])-1 or plant != input[i+1][j]:
         qty += 1
+
     print(f"Plant {plant} at {(i, j)} has per {qty}")
     return qty
-
-# def calc_perimeter(plant, input):
-#     per = 0
-#     #for p in area_perimeter_by_plant.keys():
-#     for i in range(len(input)-1):
-#         last_plant = ""
-#         for j in range(len(input[0])-1):
-#             while input[i][j] != last_plant:
-#                 last_plan = input[i][j] 
-#                 per += how_many_around(last_plan, input, i, j)
-#     return per
                     
 def calc_area_perimeter(input):
     for i in range(len(input)):
@@ -58,25 +54,50 @@ def calc_area_perimeter(input):
             while input[i][j] != last_plant:
                 last_plant = input[i][j]            
                 area = calc_area(i, j, input, t)
-                area_perimeter_by_plant[last_plant] += area
+                area_by_plant[last_plant] += area
                 
     
-    #for p in area_perimeter_by_plant.keys():
-    for i in range(len(input)-1):
-        #last_plant = ""
-        for j in range(len(input[0])-1):
+    for i in range(len(input)):
+        for j in range(len(input[0])):
             #while input[i][j] != last_plant:
-            per = 0
+            #per = 0
             last_plant = input[i][j] 
-            per = how_many_around(last_plant, input, i, j)
+            #per = how_many_around(last_plant, input, i, j)
+            perimeter_by_plant[last_plant] += how_many_around(last_plant, input, i, j)
             
-        area_perimeter_by_plant[last_plant] += per
+        #area_perimeter_by_plant[last_plant] += per
     
                 
-    print(area_perimeter_by_plant)
+    print(area_by_plant)
+    print("*" * 10)
+    print(perimeter_by_plant)
     
-    
+plant_regions = {}
+
+def regions(input, plants):
+    from pprint import pprint
+    for p in plants:
+        view = []
+        for i in range(len(input)):
+            row = []
+            for j in range(len(input[0])):                
+                if input[i][j] != p:
+                    row.append(".")
+                else:
+                    row.append(p)
+            view.append(row)
+                    #input_view = input[i][:j] + '.' + input[i][j+1:]
+                    #input_view[i][j] = '.'
+        #plant_regions[p] = input_view
+        pprint(view)
+        print()
+
+    # for each plant, delete everything which is not it.
+
 if __name__ == '__main__':
+    plants = set(input) #- '\n'
+    plants.remove('\n')
     input = input.splitlines()
-    t = transpose(input)
-    calc_area_perimeter(input)
+    #t = transpose(input)
+    regions(input, plants)
+    #calc_area_perimeter(input)
